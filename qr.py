42ec23d256f7f1d318d077b638a43ef1f414dee7
HYCCBR <n.revanth4444@gmail.com>
2025-07-06 20:01:47 +0530

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,8 @@
+import qrcode
+
+data = input("Enter the data to encode in the QR code: ")
+
+qr = qrcode.make(data)
+qr.save("qr_code.png")
+
+print("QR code generated and saved as 'qr_code.png'.")
